c97fd1640f1d559ffce9227b476b1857017c3f2c
raulbenavides27 <ra.benavides@duouc.cl>
2023-01-22 22:58:29 -0300

    omitiendo detalles de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
+ /* firebaseConfig: {
+    apiKey: "AIzaSyDC5xVvv7Lb-U-BLo5PP1XrizWWSrLMahE",
+    authDomain: "parking-db-2215.firebaseapp.com",
+    projectId: "parking-db-2215",
+    storageBucket: "parking-db-2215.appspot.com",
+    messagingSenderId: "861369138107",
+    appId: "1:861369138107:web:4edd7197746594ebcbe29e",
+    measurementId: "G-41WT1H40D1"
+  }*/
   
 };
