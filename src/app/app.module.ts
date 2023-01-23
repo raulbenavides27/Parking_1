6ad86c2ede8a1c189382c01452df30570f17e44e
raulbenavides27 <ra.benavides@duouc.cl>
2023-01-22 21:46:32 -0300

    act

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,27 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+//Firebase services + enviroments module segun profe 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from 'src/environments/environment';
+// segun documentcion 
+//import { BrowserModule } from '@angular/platform-browser';
+//import { NgModule } from '@angular/core';
+//import { IonicApp, IonicModule } from 'ionic-angular';
+//import { MyApp } from './app.component';
+//import { AngularFireModule } from '@angular/fire/compat';
+//import { firebaseConfig } from '../environment';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,AngularFireStorageModule,AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+  firebaseConfig: {
+  apiKey: "AIzaSyDC5xVvv7Lb-U-BLo5PP1XrizWWSrLMahE",
+  authDomain: "parking-db-2215.firebaseapp.com",
+  projectId: "parking-db-2215",
+  storageBucket: "parking-db-2215.appspot.com",
+  messagingSenderId: "861369138107",
+  appId: "1:861369138107:web:4edd7197746594ebcbe29e",
+  measurementId: "G-41WT1H40D1"
+}
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
